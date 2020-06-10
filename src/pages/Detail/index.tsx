48bfb91f9018cface4c16f4f1fcb77cdb24714f4
rodrigo <rodrigo.rgtic@gmail.com>
2020-06-10 16:21:10 -0300

    feat: load point detail

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { View, StyleSheet, TouchableOpacity, Image, Text, SafeAreaView } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
 import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
+import api from '../../services/api';
+
+interface Params {
+  point_id: number;
+}
+
+interface Data {
+  point: {
+    image: string;
+    name: string;
+    email: string;
+    whatsapp: string;
+    city: string;
+    uf: string;
+  }
+  items: {
+    title: string;
+  }[];
+}
 
 const Detail = () => {
+  const [data, setData] = useState<Data>({} as Data);
   const navigation = useNavigation();
+  const route = useRoute();
+
+  // Fazendo o typescript entender que o route.params tem o formato da interface Params.
+  const routeParams = route.params as Params;
+
+  useEffect(() => {
+    api.get(`points/${routeParams.point_id}`).then(response => setData(response.data));
+  },[]);
+
 
   function handleNavigateBack(){
     navigation.goBack();
@@ -19,14 +48,14 @@ const Detail = () => {
         <Icon name="arrow-left" size={20} color="#34CD79" />
       </TouchableOpacity>
 
-      <Image style={styles.pointImage} source={{ uri: 'https://media.timeout.com/images/105501076/630/472/image.jpg'}} />
+      <Image style={styles.pointImage} source={{ uri: data.point.image }} />
 
-      <Text style={styles.pointName}>Mercadão do João</Text>
-      <Text style={styles.pointItems}>Lâmpadas, Óleo de Cozinha</Text>
+      <Text style={styles.pointName}>{data.point.name}</Text>
+      <Text style={styles.pointItems}>{data.items.map(item => item.title).join(', ')}</Text>
 
       <View style={styles.address}>
-        <Text style={styles.addressTitle}>Endereço</Text>
-        <Text style={styles.addressContent}>Rio do Sul, SC</Text>
+        <Text style={styles.addressTitle}>{data.point.city}</Text>
+        <Text style={styles.addressContent}>{data.point.uf}</Text>
       </View>
     </View>
     <View style={styles.footer}>
